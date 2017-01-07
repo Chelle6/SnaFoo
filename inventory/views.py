f619196359abf92fc33c5d8a0803873b5b489d52
Michelle <b.michelle6@gmail.com>
2017-01-06 20:45:49 -0800

    Added polls completed inventory

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def index(request):
+	items = Item.objects.exclude(amount=0)
+	return render(request, 'inventory/index.html', {
+		'items': items,
+	})
+
+
+	return HttpResponse('<p>In index view</p>')
