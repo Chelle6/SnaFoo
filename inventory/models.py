0df4a93fb4d8e8ae2dbd49981dd2bffa25c93cf6
Michelle <b.michelle6@gmail.com>
2017-01-22 19:49:19 -0800

    JSON reading implemented

--- inventory/models.py
+++ inventory/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+from django.db import models
+
 class snack(models.Model):
 	name = models.TextField()  # string
 	optional = models.BooleanField()  # boolean
-	purchaseLocations = models.TextField()  # string
-	purchaseCount = models.IntegerField()  # integer
-	lastPurchaseDate = models.TextField()  # string
+	purchaseLocations = models.TextField(default="None Listed")  # string
+	purchaseCount = models.IntegerField(default=0)  # integer
+	lastPurchaseDate = models.TextField(null=True, default="Never Purchased")  # string 
 	votes = models.IntegerField(default=0)
--- inventory/views.py
+++ inventory/views.py
@@ -1,14 +1,47 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from inventory.models import snack
+from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
 from django.core.urlresolvers import reverse
+import json
+from inventory.models import snack
+#import urllib
+# from . import forms
+try:
+	import urllib.request as urllib2
+except ImportError:
+	import urllib2
+
+# import urllib
+# # import urllib2
+# import json
+# import requests
+# from django.http import JsonResponse
+
+api_key = '24f93691-f294-4165-b731-660c160b1919'
+url = 'https://api-snacks.nerderylabs.com/v1/snacks?ApiKey=' + api_key
+# json_obj = urllib2.urlopen(url)
+# data = json.loads(json_obj.read())
+
+# alwaysPurchasedSnacks = []
+# suggestedSnacks = []
+
+# for item in data:
+# 	if item['optional'] == False:
+# 		suggestedSnacks.append(item)
+# 	else:
+# 		alwaysPurchasedSnacks.append(item)
 
 def index(request):
-	#items = snack.objects.all()
-	
+	json_obj = urllib2.urlopen(url) # get json data from Web API
+	data = json.loads(json_obj.read().decode('utf8'))  # convert json data to Python dictionary
+
+	for item in data:
+		if not snack.objects.filter(name=item['name']).exists(): # if the object doesn't exist
+			s = snack(name=item['name'], optional=item['optional'], purchaseLocations=item['purchaseLocations'], purchaseCount=item['purchaseCount'], lastPurchaseDate=item['lastPurchaseDate'], id=item['id'])
+			s.save()
+	# votesRemaining = 3
+
 	alwaysPurchasedSnacks = snack.objects.filter(optional=False)
 	suggestedSnacks = snack.objects.filter(optional=True)
-
 
 	return render(request, 'inventory/index.html', {
 		#'items': items,
